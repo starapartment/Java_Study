434cecdf2942aaaa698521ab6b47c40e30d6f263
DannyTsai <41992416+starapartment@users.noreply.github.com>
2019-09-10 22:28:34 +0800

    Add files via upload

---
+++ src/ConditionalOperator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+public class ConditionalOperator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入數字: "); 
+        int input = scanner.nextInt();
+        int remain = input % 2; // 求除 2 的餘數
+ 
+        if(remain == 1) { // 如果餘數為1
+        	System.out.println("插個花");
+        	System.out.println(input + "為奇數"); } 
+        else {
+        	System.out.println("插個花");
+            System.out.println(input + "為偶數"); } 
+    }
+}
+
+
+
+
